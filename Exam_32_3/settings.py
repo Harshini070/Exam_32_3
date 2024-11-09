a1a7fb0f75e3c360aa9b8f3a2fc997142878d2a5
Harshini070 <Sree Harshini>
2024-11-09 19:22:33 +0530

    Initial Commit

--- Exam_32_3/settings.py
+++ Exam_32_3/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-czzy&lr*#3tz%mt_#+&1+!vvaafnp*xjs0zdu_p0-njboa+*_7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['Exam_32_3.onrender.com','127.0.0.1']
+ALLOWED_HOSTS = ['Exam_32_3-5.onrender.com','127.0.0.1']
 
 
 # Application definition
